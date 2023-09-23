af056f4436c8d84c3cd8406dc9f2a463ca1d0291
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-09-23 20:00:52 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-function App() {
-  return <div>
-    <h1>App</h1>
-  </div>;
-}
+import { Home } from "./pages/home";
+import { Cart } from "./pages/cart";
+import { ErrorPage } from "./pages/error";
+import { Layout } from "./components/layout";
+import { createBrowserRouter } from "react-router-dom";
 
-export default App;
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
+
+export { router };
