5f2572c5ab7c30205bc711f5b02ac1a24a744a02
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-09-24 21:27:17 -0300

    feat: add react hot toast

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import { router } from "./App.tsx";
 import "./index.css";
 
 import CartProvider from "./contexts/CartContext.tsx";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CartProvider>
+      <Toaster position="bottom-center" reverseOrder={false} />
       <RouterProvider router={router} />
     </CartProvider>
   </React.StrictMode>
