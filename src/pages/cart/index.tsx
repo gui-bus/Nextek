cabc66e141ec44659707919f5aaf57e8b9e7a5af
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-09-24 21:28:54 -0300

    style: update home & cart styles

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,9 +1,17 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
+
 import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
 export function Cart() {
+  const { cart, total, addItemCart, removeItemCart, clearCart } =
+    useContext(CartContext);
+
   return (
     <div className="w-full max-w-2xl md:max-w-7xl mx-auto px-4">
       {/* Banner */}
-      <div className="bg-cover bg-center bg-zinc-700 text-white py-1 rounded-bl-[6rem] rounded-br-[6rem] md:rounded-bl-full md:rounded-br-full">
+      <div className="bg-cover bg-center bg-gradient-to-b from-zinc-600 to-zinc-800 text-white py-1 rounded-bl-[6rem] rounded-br-[6rem] md:rounded-bl-full md:rounded-br-full">
         <h1 className="text-lg md:text-4xl font-semibold my-4 text-gray-100 text-center">
           Carrinho de Compras
         </h1>
@@ -12,47 +20,109 @@ export function Cart() {
         </p>
       </div>
 
+      {cart.length === 0 && (
+        <div className="flex w-full justify-center items-center flex-col mt-4 md:mt-10">
+          <div className="my-8 text-center px-4">
+            <h1 className="font-bold text-2xl">
+              Oops! Seu carrinho está vazio...
+            </h1>
+            <p className="mt-2">
+              Não se preocupe, você pode começar a adicionar itens agora!
+            </p>
+          </div>
+
+          <Link
+            to="/"
+            className="flex items-center justify-center gap-2 h-9 text-white bg-nextek rounded-md border-0 font-medium w-11/12 max-w-lg px-4 py-2 transition-all duration-300 ease-in-out hover:bg-nextekHover"
+          >
+            Ver lista de produtos
+          </Link>
+        </div>
+      )}
+
       {/* Cart Product GRID */}
       <div className="my-5 grid grid-cols-1 gap-6 md:grid-cols-2">
         {/* Cart Product */}
-        <section className="flex items-center justify-between drop-shadow-md bg-zinc-700 p-4 text-white rounded transition-all duration-200 ease-linear hover:scale-[1.02] cursor-pointer">
-          <img
-            className="w-28 rounded-lg object-cover"
-            src="https://images.tcdn.com.br/img/img_prod/993382/console_playstation_5_c_drive_2_controles_dualsense_brancos_headset_pulse_ps5_485_1_e7c644f61791fcafeb970f0989c7bfea.jpg"
-            alt="Produto"
-          />
-          <div className="flex items-center justify-center flex-col md:flex-row md:gap-5 px-3 md:px-5">
-            <div className="flex flex-col items-center justify-center">
-              <p className="flex items-center justify-center">Valor unitário</p>
-              <p className=" font-medium text-white">R$ 4.700,00</p>
-            </div>
+        {cart.map((item) => (
+          <section
+            key={item.id}
+            className="flex flex-col items-center justify-between drop-shadow-md bg-gradient-to-b from-zinc-600 to-zinc-800 p-4 text-white rounded  cursor-default select-none"
+          >
+            <p className="font-medium mb-2 text-sm text-white text-center">
+              {item.title}
+            </p>
+            <div className="h-px border-b border-white w-full max-w-7xl"></div>
+            <div className="flex flex-col space-y-5 lg:space-y-0 lg:flex-row items-center justify-between p-4">
+              <Link to={`/product/${item.id}`}>
+                <img
+                  className="w-28 h-28 rounded-lg object-contain bg-white p-2 transition-all duration-200 ease-linear hover:scale-[1.02]"
+                  src={item.image}
+                  alt={item.title}
+                />
+              </Link>
+              <div className="flex items-center justify-center flex-col md:flex-row md:gap-5 px-3 md:px-5">
+                <div className="flex flex-col items-center justify-center">
+                  <p className="flex items-center justify-center">
+                    Valor unitário
+                  </p>
+                  <p className=" font-medium text-white">
+                    {(item.price * 4.9).toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </p>
+                </div>
 
-            <div className="flex items-center justify-center gap-3 my-2">
-              <button className="bg-nextek p-1 rounded-full transition-all duration-200 ease-linear hover:bg-nextekHover flex items-center justify-center">
-                <AiOutlineMinusCircle size={28} color="#fff" />
-              </button>
-              10
-              <button className="bg-nextek p-1 rounded-full transition-all duration-200 ease-linear hover:bg-nextekHover flex items-center justify-center">
-                <AiOutlinePlusCircle size={28} color="#fff" />
-              </button>
-            </div>
+                <div className="flex items-center justify-center gap-3 my-2">
+                  <button
+                    className="bg-nextek p-1 rounded-full transition-all duration-200 ease-linear hover:bg-nextekHover flex items-center justify-center"
+                    onClick={() => removeItemCart(item)}
+                  >
+                    <AiOutlineMinusCircle size={28} color="#fff" />
+                  </button>
+                  {item.amount}
+                  <button
+                    className="bg-nextek p-1 rounded-full transition-all duration-200 ease-linear hover:bg-nextekHover flex items-center justify-center"
+                    onClick={() => addItemCart(item)}
+                  >
+                    <AiOutlinePlusCircle size={28} color="#fff" />
+                  </button>
+                </div>
 
-            <div className="flex md:flex-col items-center justify-center gap-1">
-              <p className="flex items-center justify-center">
-                SubTotal <span className="md:hidden">:</span>
-              </p>
-              <p className="font-medium text-white">R$ 4.700,00</p>
+                <div className="flex md:flex-col items-center justify-center gap-1">
+                  <p className="flex items-center justify-center">
+                    SubTotal <span className="md:hidden">:</span>
+                  </p>
+                  <p className="font-medium text-white">
+                    {item.total.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        ))}
       </div>
 
       {/* Total */}
-      <div className="h-px border-b border-white w-full max-w-7xl my-5">
-        <span className="float-right mt-4 font-medium text-white">
-          Total: R$ 4.700,00
-        </span>
-      </div>
+      {cart.length !== 0 && (
+        <div className="w-full">
+          <div className="h-px border-b border-white w-full max-w-7xl my-5">
+            <span className="float-right mt-4 font-medium text-white">
+              Total: {total}
+            </span>
+
+            <span
+              className="float-left mt-4 font-medium text-white cursor-pointer transition duration-300 ease-linear hover:underline"
+              onClick={() => clearCart()}
+            >
+              Limpar carrinho
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,27 +5,39 @@ import { TbShoppingCartPlus } from "react-icons/tb";
 
 import { CartContext } from "../../contexts/CartContext";
 
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+
 export interface ProductProps {
   id: number;
   title: string;
   description: string;
   price: number;
-  cover: string;
+  image: string;
+  category: string;
 }
 
 export function Home() {
   const { addItemCart } = useContext(CartContext);
   const [products, setProducts] = useState<ProductProps[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getProducts() {
-      const response = await api.get("/products");
-      setProducts(response.data);
+      try {
+        const response = await api.get("/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+      } finally {
+        setLoading(false);
+      }
     }
     getProducts();
   }, []);
 
   function handleAddCartItem(product: ProductProps) {
+    toast.success("Produto adicionado ao carrinho!");
     addItemCart(product);
   }
 
@@ -33,51 +45,208 @@ export function Home() {
     <div>
       <main className="w-full max-w-2xl md:max-w-7xl mx-auto px-4">
         {/* Banner */}
-        <div className="bg-cover bg-center bg-zinc-700 text-white py-1 rounded-bl-[6rem] rounded-br-[6rem] md:rounded-bl-full md:rounded-br-full">
-          <h1 className="text-lg md:text-4xl font-semibold my-4 text-gray-100 text-center">
-            A melhor seleção de produtos!
+        <div className="bg-cover bg-center bg-gradient-to-b from-zinc-600 to-zinc-800 text-white py-1 rounded-bl-[6rem] rounded-br-[6rem] md:rounded-bl-full md:rounded-br-full">
+          <h1 className="text-lg md:text-3xl font-semibold my-4 text-gray-100 text-center">
+            Explore a Variedade de Itens!
           </h1>
           <p className="text-sm text-gray-200 text-center mb-8 px-4">
-            Gadgets e Tecnologia Premium.
+            Eletrônicos, Moda e Joias Exclusivas
           </p>
         </div>
 
-        {/* Product */}
-        <div className="my-5 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-          {products.map((product) => (
-            <section
-              key={product.id}
-              className="w-full transition-all duration-200 ease-linear hover:scale-[1.02] px-2 md:px-0 cursor-pointer"
-            >
-              <img
-                className="w-full rounded-t-lg max-h-70 object-cover"
-                src={product.cover}
-                alt={product.title}
-              />
+        {loading ? (
+          <div className="flex items-center justify-center my-28">
+            <div className="w-16 h-16 border-t-4 border-tr-4 border-nextek rounded-full animate-spin"></div>
+          </div>
+        ) : (
+          <section>
+            <p className="mt-4 mb-2 font-medium text-white text-center text-xl bg-gradient-to-r from-nextek to-nextekHover py-1 rounded-tr-full rounded-tl-full drop-shadow-xl">
+              Eletrônicos
+            </p>
 
-              <div className="bg-zinc-700 rounded-b-lg p-2 h-24">
-                <p className="font-medium mb-2 text-sm md:text-xs text-white h-8">
-                  {product.title}
-                </p>
-
-                <div className="flex gap-3 items-center justify-between px-2">
-                  <strong className="text-white">
-                    {product.price.toLocaleString("pt-BR", {
-                      style: "currency",
-                      currency: "BRL",
-                    })}
-                  </strong>
-                  <button
-                    className="bg-nextek p-1 rounded transition-all duration-200 ease-linear hover:bg-nextekHover"
-                    onClick={() => handleAddCartItem(product)}
+            {/* Product electronics */}
+            <div className="my-5 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-6">
+              {products
+                .filter((product) => product.category === "electronics")
+                .map((product) => (
+                  <section
+                    key={product.id}
+                    className="w-full transition-all duration-200 ease-linear hover:scale-[1.02] px-2 md:px-0 cursor-default select-none"
                   >
-                    <TbShoppingCartPlus size={28} color="#fff" />
-                  </button>
-                </div>
-              </div>
-            </section>
-          ))}
-        </div>
+                    <Link to={`/product/${product.id}`}>
+                      <img
+                        className="w-full rounded-t-lg h-72 object-contain bg-white p-5 cursor-pointer"
+                        src={product.image}
+                        alt={product.title}
+                      />
+                    </Link>
+
+                    <div className="bg-gradient-to-b from-zinc-600 to-zinc-800 rounded-b-lg p-2 h-32 lg:h-40">
+                      <p className="px-2 font-medium mb-2 text-sm md:text-xs text-white h-14 lg:h-[5.5rem]">
+                        {product.title}
+                      </p>
+
+                      <div className="flex gap-3 items-center justify-between px-2">
+                        <strong className="text-white">
+                          {(product.price * 4.9).toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                          })}
+                        </strong>
+                        <button
+                          className="bg-nextek p-1 rounded transition-all duration-200 ease-linear hover:bg-nextekHover"
+                          onClick={() => handleAddCartItem(product)}
+                        >
+                          <TbShoppingCartPlus size={28} color="#fff" />
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+                ))}
+            </div>
+
+            <div className="h-px border-b border-white w-full max-w-7xl mb-5"></div>
+
+            <p className="mt-4 mb-2 font-medium text-white text-center text-xl bg-gradient-to-r from-nextek to-nextekHover py-1 rounded-tr-full rounded-tl-full drop-shadow-xl">
+              Moda feminina
+            </p>
+
+            {/* Product women's clothing */}
+            <div className="my-5 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-6">
+              {products
+                .filter((product) => product.category === "women's clothing")
+                .map((product) => (
+                  <section
+                    key={product.id}
+                    className="w-full transition-all duration-200 ease-linear hover:scale-[1.02] px-2 md:px-0 cursor-default select-none"
+                  >
+                    <Link to={`/product/${product.id}`}>
+                      <img
+                        className="w-full rounded-t-lg h-72 object-contain bg-white p-5 cursor-pointer"
+                        src={product.image}
+                        alt={product.title}
+                      />
+                    </Link>
+
+                    <div className="bg-gradient-to-b from-zinc-600 to-zinc-800 rounded-b-lg p-2 h-32">
+                      <p className="px-2 font-medium mb-2 text-sm md:text-xs text-white h-14">
+                        {product.title}
+                      </p>
+
+                      <div className="flex gap-3 items-center justify-between px-2">
+                        <strong className="text-white">
+                          {(product.price * 4.9).toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                          })}
+                        </strong>
+                        <button
+                          className="bg-nextek p-1 rounded transition-all duration-200 ease-linear hover:bg-nextekHover"
+                          onClick={() => handleAddCartItem(product)}
+                        >
+                          <TbShoppingCartPlus size={28} color="#fff" />
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+                ))}
+            </div>
+
+            <div className="h-px border-b border-white w-full max-w-7xl mb-5"></div>
+
+            <p className="mt-4 mb-2 font-medium text-white text-center text-xl bg-gradient-to-r from-nextek to-nextekHover py-1 rounded-tr-full rounded-tl-full drop-shadow-xl">
+              Moda masculina
+            </p>
+
+            {/* Product mens's clothing */}
+            <div className="my-5 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+              {products
+                .filter((product) => product.category === "men's clothing")
+                .map((product) => (
+                  <section
+                    key={product.id}
+                    className="w-full transition-all duration-200 ease-linear hover:scale-[1.02] px-2 md:px-0 cursor-default select-none"
+                  >
+                    <Link to={`/product/${product.id}`}>
+                      <img
+                        className="w-full rounded-t-lg h-72 object-contain bg-white p-5 cursor-pointer"
+                        src={product.image}
+                        alt={product.title}
+                      />
+                    </Link>
+
+                    <div className="bg-gradient-to-b from-zinc-600 to-zinc-800 rounded-b-lg p-2 h-24">
+                      <p className="px-2 font-medium mb-2 text-sm md:text-xs text-white h-7">
+                        {product.title}
+                      </p>
+
+                      <div className="flex gap-3 items-center justify-between px-2">
+                        <strong className="text-white">
+                          {(product.price * 4.9).toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                          })}
+                        </strong>
+                        <button
+                          className="bg-nextek p-1 rounded transition-all duration-200 ease-linear hover:bg-nextekHover"
+                          onClick={() => handleAddCartItem(product)}
+                        >
+                          <TbShoppingCartPlus size={28} color="#fff" />
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+                ))}
+            </div>
+
+            <div className="h-px border-b border-white w-full max-w-7xl mb-5"></div>
+
+            <p className="mt-4 mb-2 font-medium text-white text-center text-xl bg-gradient-to-r from-nextek to-nextekHover py-1 rounded-tr-full rounded-tl-full drop-shadow-xl">
+              Joalheria
+            </p>
+
+            {/* Product jewelery */}
+            <div className="my-5 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+              {products
+                .filter((product) => product.category === "jewelery")
+                .map((product) => (
+                  <section
+                    key={product.id}
+                    className="w-full transition-all duration-200 ease-linear hover:scale-[1.02] px-2 md:px-0 cursor-default select-none"
+                  >
+                    <Link to={`/product/${product.id}`}>
+                      <img
+                        className="w-full rounded-t-lg h-72 object-contain bg-white p-5 cursor-pointer"
+                        src={product.image}
+                        alt={product.title}
+                      />
+                    </Link>
+
+                    <div className="bg-gradient-to-b from-zinc-600 to-zinc-800 rounded-b-lg p-2 h-[6.5rem]">
+                      <p className="px-2 font-medium mb-2 text-sm md:text-xs text-white h-9">
+                        {product.title}
+                      </p>
+
+                      <div className="flex gap-3 items-center justify-between px-2">
+                        <strong className="text-white">
+                          {(product.price * 4.9).toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                          })}
+                        </strong>
+                        <button
+                          className="bg-nextek p-1 rounded transition-all duration-200 ease-linear hover:bg-nextekHover"
+                          onClick={() => handleAddCartItem(product)}
+                        >
+                          <TbShoppingCartPlus size={28} color="#fff" />
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+                ))}
+            </div>
+          </section>
+        )}
       </main>
     </div>
   );
